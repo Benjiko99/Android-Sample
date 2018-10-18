d0ab3d6de38fd05aa9d74519f26812bfe9cceac4
Tomáš Beňo <benjiko99@gmail.com>
2018-10-18 16:21:27 +0200

    Setup navigation

--- app/src/main/java/com/spiraclesoftware/airbankinterview/MainActivity.kt
+++ app/src/main/java/com/spiraclesoftware/airbankinterview/MainActivity.kt
@@ -2,11 +2,21 @@ package com.spiraclesoftware.airbankinterview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.main__activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main__activity)
+
+        setSupportActionBar(toolbar)
+        NavigationUI.setupActionBarWithNavController(this, findNavController(R.id.navHostFragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(null, findNavController(R.id.navHostFragment))
     }
 }
