93c517149ecb8562e6800594a1441777a58d635e
Tomáš Beňo <benjiko99@gmail.com>
2018-10-23 09:30:33 +0200

    Add TransactionDetail screen

---
+++ app/src/main/java/com/spiraclesoftware/airbankinterview/common/domain/Account.kt
@@ -0,0 +1,7 @@
+package com.spiraclesoftware.airbankinterview.common.domain
+
+data class Account(
+    val accountNumber: String,
+    val accountName: String,
+    val bankCode: String
+)
---
+++ app/src/main/java/com/spiraclesoftware/airbankinterview/transaction/detail/data/TransactionDetailCache.kt
@@ -0,0 +1,25 @@
+package com.spiraclesoftware.airbankinterview.transaction.detail.data
+
+import com.spiraclesoftware.airbankinterview.transaction.detail.domain.TransactionDetail
+import com.spiraclesoftware.airbankinterview.transaction.list.domain.TransactionId
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TransactionDetailCache @Inject constructor() {
+
+    /** Marks the cache as invalid, to force an update the next time data is requested. */
+    var cacheIsDirty = false
+
+    private var cache: HashMap<TransactionId, TransactionDetail>? = null
+
+    fun set(id: TransactionId, data: TransactionDetail) {
+        if (cache == null) {
+            cache = LinkedHashMap()
+        }
+        cache!![id] = data
+        cacheIsDirty = false
+    }
+
+    fun get(id: TransactionId): TransactionDetail? = cache?.get(id)
+}
