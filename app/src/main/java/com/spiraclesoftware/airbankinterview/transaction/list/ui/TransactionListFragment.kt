12bffd5cd2d0d6955874ecebf28ed4c6cfc53f8d
Tomáš Beňo <benjiko99@gmail.com>
2018-10-25 22:23:50 +0200

    Refactor Fragments

--- app/src/main/java/com/spiraclesoftware/airbankinterview/transaction/list/ui/TransactionListFragment.kt
+++ app/src/main/java/com/spiraclesoftware/airbankinterview/transaction/list/ui/TransactionListFragment.kt
@@ -11,8 +11,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
-import com.spiraclesoftware.airbankinterview.shared.ui.RetryCallback
 import com.spiraclesoftware.airbankinterview.databinding.TransactionListFragmentBinding
+import com.spiraclesoftware.airbankinterview.shared.ui.RetryCallback
 import com.spiraclesoftware.airbankinterview.transaction.list.data.TransactionDirectionFilter
 import com.spiraclesoftware.airbankinterview.transaction.list.domain.Transaction
 import com.spiraclesoftware.airbankinterview.transaction.list.ui.TransactionListFragmentDirections.actionTransactionListFragmentToTransactionDetailFragment
@@ -34,47 +34,65 @@ class TransactionListFragment : DaggerFragment() {
         binding = TransactionListFragmentBinding.inflate(inflater)
 
         binding.retryCallback = object : RetryCallback {
-            override fun retry() = viewModel.retry()
+            override fun retry() {
+                viewModel.retry()
+            }
         }
 
         return binding.root
     }
 
+    // NOTE: We're setting up our views here instead of in onCreateView() because we're using Kotlin Synthetic Properties
+    // to bind our views which requires the fragment's view to be attached.
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fastItemAdapter = FastItemAdapter()
-        fastItemAdapter.apply {
-            withSelectable(true)
-            withOnClickListener { _, _, item, _ ->
-                findNavController().navigate(
-                    actionTransactionListFragmentToTransactionDetailFragment(item.transaction.id)
-                )
-                true
+        fun setupFastItemAdapter() {
+            fastItemAdapter = FastItemAdapter()
+            fastItemAdapter.apply {
+                withSelectable(true)
+                withOnClickListener { _, _, item, _ ->
+                    findNavController().navigate(
+                        actionTransactionListFragmentToTransactionDetailFragment(item.transaction.id)
+                    )
+                    true
+                }
             }
         }
 
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = fastItemAdapter
-            itemAnimator = null
+        fun setupRecyclerView() {
+            recyclerView.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = fastItemAdapter
+                itemAnimator = null
+            }
         }
 
-        val filterSpinnerAdapter = ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            TransactionDirectionFilter.values().map { getString(it.stringRes) }
-        )
-        filterSpinner.adapter = filterSpinnerAdapter
-        filterSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val transactionDirectionFilter = TransactionDirectionFilter.values()[position]
-                viewModel.setTransactionDirectionFilter(transactionDirectionFilter)
+        fun setupFilterSpinner() {
+
+            fun createSpinnerAdapter(): ArrayAdapter<String> = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                TransactionDirectionFilter.values().map { getString(it.stringRes) }
+            ).also {
+                it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            filterSpinner.adapter = createSpinnerAdapter()
+
+            filterSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val transactionDirectionFilter = TransactionDirectionFilter.values()[position]
+                    viewModel.setTransactionDirectionFilter(transactionDirectionFilter)
+                }
+            }
         }
-        filterSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        setupFastItemAdapter()
+        setupRecyclerView()
+        setupFilterSpinner()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -82,6 +100,10 @@ class TransactionListFragment : DaggerFragment() {
 
         viewModel = viewModelProvider(viewModelFactory)
 
+        subscribeUi()
+    }
+
+    private fun subscribeUi() {
         viewModel.transactions.observe(this, Observer { resource ->
             binding.transactionListResource = resource
             fastItemAdapter.set(toListItems(resource.data))
