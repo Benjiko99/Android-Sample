e06105875bf3c9506810a813aa80d81fe1373ae8
Tomáš Beňo <benjiko99@gmail.com>
2018-11-19 00:19:23 +0100

    Rename binding adapter from visibleGone to goneUnless

--- core/src/main/java/com/spiraclesoftware/core/extensions/BindingAdapters.kt
+++ core/src/main/java/com/spiraclesoftware/core/extensions/BindingAdapters.kt
@@ -6,8 +6,8 @@ import androidx.databinding.BindingAdapter
 object BindingAdapters {
 
     @JvmStatic
-    @BindingAdapter("app:visibleGone")
-    fun visibleGone(view: View, show: Boolean) {
+    @BindingAdapter("goneUnless")
+    fun goneUnless(view: View, show: Boolean) {
         view.visibility = if (show) View.VISIBLE else View.GONE
     }
 }
