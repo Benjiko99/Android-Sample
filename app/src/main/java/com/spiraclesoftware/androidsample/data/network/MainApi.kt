57d01e97777ad07028133bf1118efa0074520d26
Tomáš Beňo <benjiko99@gmail.com>
2020-07-16 13:21:53 +0200

    Remove unnecessary header from api call

--- app/src/main/java/com/spiraclesoftware/androidsample/data/network/MainApi.kt
+++ app/src/main/java/com/spiraclesoftware/androidsample/data/network/MainApi.kt
@@ -3,12 +3,10 @@ package com.spiraclesoftware.androidsample.data.network
 import com.spiraclesoftware.androidsample.data.network.model.TransactionsResponseWrapper
 import com.spiraclesoftware.androidsample.domain.model.ConversionRates
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface MainApi {
 
-    @Headers("Accept: application/json")
     @GET("transactions")
     suspend fun fetchTransactions(): TransactionsResponseWrapper
 
