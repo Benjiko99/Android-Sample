a2065d78e71be51f89da77bd588bfaf49452f442
Tomáš Beňo <benjiko99@gmail.com>
2020-07-16 10:04:44 +0200

    Remove unused AuthInterceptor but keep it as a reference in the docs

--- app/src/main/java/com/spiraclesoftware/androidsample/data/network/interceptor/AuthInterceptor.kt
+++
@@ -1,9 +0,0 @@
-package com.spiraclesoftware.androidsample.data.network.interceptor
-
-import okhttp3.HttpUrl
-
-class AuthInterceptor : UrlInterceptor() {
-    override fun HttpUrl.Builder.update(url: HttpUrl) {
-        //addQueryParameter("api-key", BuildConfig.API_KEY)
-    }
-}
--- app/src/main/java/com/spiraclesoftware/androidsample/data/network/interceptor/UrlInterceptor.kt
+++ app/src/main/java/com/spiraclesoftware/androidsample/data/network/interceptor/UrlInterceptor.kt
@@ -4,6 +4,18 @@ import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Response
 
+/**
+ * Simplifies adding parameters to intercepted requests.
+ *
+ * Example of adding an api-key to requests:
+ * ```
+ * class AuthInterceptor : UrlInterceptor() {
+ *     override fun HttpUrl.Builder.update(url: HttpUrl) {
+ *         addQueryParameter("api-key", BuildConfig.API_KEY)
+ *     }
+ * }
+ * ```
+ */
 abstract class UrlInterceptor : Interceptor {
 
     final override fun intercept(chain: Interceptor.Chain): Response {
@@ -11,13 +23,8 @@ abstract class UrlInterceptor : Interceptor {
         val url = request.url
         val newRequest = request.newBuilder()
             .url(
-                url.newBuilder()
-                    .apply {
-                        update(url)
-                    }
-                    .build()
-            )
-            .build()
+                url.newBuilder().apply { update(url) }.build()
+            ).build()
         return chain.proceed(newRequest)
     }
 
