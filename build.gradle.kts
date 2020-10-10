76f95230803c887366fd576478b18900a63f3ba3
Tomáš Beňo <benjiko99@gmail.com>
2020-10-10 18:11:03 +0200

    Update gradle tools 4.0.1 -> 4.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.0.2")
         classpath(kotlin("gradle-plugin", version = Dependencies.kotlin))
         classpath("org.jetbrains.kotlin:kotlin-allopen:${Dependencies.kotlin}")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Dependencies.navigation}")
