8bad68abe8953c206dc3dfba81910ae235fe7e60
Tomáš Beňo <benjiko99@gmail.com>
2021-02-24 16:36:13 +0100

    Add tests

--- app/src/test/java/com/spiraclesoftware/androidsample/feature/transaction_list/TransactionListViewModelTest.kt
+++ app/src/test/java/com/spiraclesoftware/androidsample/feature/transaction_list/TransactionListViewModelTest.kt
@@ -5,10 +5,13 @@ import co.zsmb.rainbowcake.test.base.ViewModelTest
 import co.zsmb.rainbowcake.test.observeStateAndEvents
 import com.google.common.truth.Truth.assertThat
 import com.spiraclesoftware.androidsample.domain.Result
+import com.spiraclesoftware.androidsample.domain.entity.TransactionId
+import com.spiraclesoftware.androidsample.feature.transaction_list.TransactionListFragmentDirections.Companion.toTransactionDetail
+import com.spiraclesoftware.androidsample.feature.transaction_list.TransactionListViewModel.NavigateEvent
+import com.spiraclesoftware.androidsample.feature.transaction_list.TransactionListViewModel.ShowLanguageChangeConfirmationEvent
 import com.spiraclesoftware.androidsample.feature.transaction_list.TransactionListViewState.Content
 import com.spiraclesoftware.androidsample.framework.Model
-import io.mockk.coEvery
-import io.mockk.mockk
+import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
@@ -18,15 +21,76 @@ import org.junit.Test
 class TransactionListViewModelTest : ViewModelTest() {
 
     @Test
+    fun openTransactionDetail() {
+        val presenter: TransactionListPresenter = mockk()
+        val testSubject = TransactionListViewModel(presenter)
+
+        testSubject.observeStateAndEvents { _, eventsObserver ->
+            testSubject.openTransactionDetail(TransactionId("1"))
+
+            eventsObserver.assertObserved(
+                NavigateEvent(toTransactionDetail("1"))
+            )
+        }
+    }
+
+    @Test
+    fun changeLanguage() {
+        val presenter: TransactionListPresenter = mockk()
+        val testSubject = TransactionListViewModel(presenter)
+
+        testSubject.observeStateAndEvents { _, eventsObserver ->
+            testSubject.changeLanguage()
+
+            eventsObserver.assertObserved(
+                ShowLanguageChangeConfirmationEvent
+            )
+        }
+    }
+
+    @Test
+    fun confirmLanguageChange() {
+        val presenter: TransactionListPresenter = mockk {
+            justRun { toggleLanguageAndRestart() }
+        } 
+        val testSubject = TransactionListViewModel(presenter)
+
+        testSubject.confirmLanguageChange()
+        verify { presenter.toggleLanguageAndRestart() }
+    }
+
+    @Test
+    fun refreshData() = runBlockingTest {
+        val presenter: TransactionListPresenter = mockk {
+            coJustRun { refreshTransactions() }
+        }
+        val testSubject = TransactionListViewModel(presenter)
+
+        testSubject.refreshData()
+        coVerify { presenter.refreshTransactions() }
+    }
+
+    @Test
+    fun retryOnError() = runBlockingTest {
+        val presenter: TransactionListPresenter = mockk {
+            coJustRun { refreshTransactions() }
+        }
+        val testSubject = TransactionListViewModel(presenter)
+
+        testSubject.retryOnError()
+        coVerify { presenter.refreshTransactions() }
+    }
+
+    @Test
     fun `Data is loaded correctly from presenter upon creation and leads to ready state`() = runBlockingTest {
         val presenter: TransactionListPresenter = mockk()
         val models = listOf(mockk<Model>())
 
         coEvery { presenter.flowListModels(any()) } returns flowOf(Result.Success(models))
 
-        val vm = TransactionListViewModel(presenter)
+        val testSubject = TransactionListViewModel(presenter)
 
-        vm.observeStateAndEvents { stateObserver, _ ->
+        testSubject.observeStateAndEvents { stateObserver, _ ->
             stateObserver.assertObserved(
                 Content(
                     models,
@@ -41,9 +105,9 @@ class TransactionListViewModelTest : ViewModelTest() {
         val presenter: TransactionListPresenter = mockk()
         coEvery { presenter.flowListModels(any()) } returns flowOf(Result.Success(emptyList()))
 
-        val vm = TransactionListViewModel(presenter)
+        val testSubject = TransactionListViewModel(presenter)
 
-        vm.observeStateAndEvents { stateObserver, _ ->
+        testSubject.observeStateAndEvents { stateObserver, _ ->
             val viewState = stateObserver.observed.first() as Content
 
             assertThat(viewState.listModels).isEmpty()
