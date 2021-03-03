81c3ed1c5a2164b34fb471ceb62ca3138eac7a08
Tomáš Beňo <benjiko99@gmail.com>
2021-03-03 13:35:32 +0100

    Remove unused extensions

--- app/src/main/java/com/spiraclesoftware/androidsample/extension/ViewExtensions.kt
+++ app/src/main/java/com/spiraclesoftware/androidsample/extension/ViewExtensions.kt
@@ -11,24 +11,6 @@ var View.topMargin: Int?
     set(marginPx) {
         (layoutParams as? LinearLayout.LayoutParams)?.topMargin = marginPx
     }
-
-var View.rightMargin: Int?
-    get() = (layoutParams as? LinearLayout.LayoutParams)?.rightMargin
-    set(marginPx) {
-        (layoutParams as? LinearLayout.LayoutParams)?.rightMargin = marginPx
-    }
-
-var View.bottomMargin: Int?
-    get() = (layoutParams as? LinearLayout.LayoutParams)?.bottomMargin
-    set(marginPx) {
-        (layoutParams as? LinearLayout.LayoutParams)?.bottomMargin = marginPx
-    }
-
-var View.leftMargin: Int?
-    get() = (layoutParams as? LinearLayout.LayoutParams)?.leftMargin
-    set(marginPx) {
-        (layoutParams as? LinearLayout.LayoutParams)?.leftMargin = marginPx
-    }
 //endregion
 
 //region TextView
