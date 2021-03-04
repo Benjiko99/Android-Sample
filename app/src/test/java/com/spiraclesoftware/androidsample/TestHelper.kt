b88d4ef5a3f27e489d773107ac7e5e457a838ba9
Tomáš Beňo <benjiko99@gmail.com>
2021-03-04 16:13:53 +0100

    Add test helper for creating Transaction instances

--- app/src/test/java/com/spiraclesoftware/androidsample/TestHelper.kt
+++ app/src/test/java/com/spiraclesoftware/androidsample/TestHelper.kt
@@ -1,6 +1,6 @@
 package com.spiraclesoftware.androidsample
 
-import com.spiraclesoftware.androidsample.domain.entity.Money
+import com.spiraclesoftware.androidsample.domain.entity.*
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 import java.util.*
@@ -11,3 +11,29 @@ val epochDateTime = ZonedDateTime.parse("1970-01-01T00:00:00+00:00")!!
 fun money(amount: String, currencyCode: String): Money {
     return Money(BigDecimal(amount), Currency.getInstance(currencyCode))
 }
+
+fun transaction(
+    id: TransactionId = TransactionId("1"),
+    name: String = "Transaction #${id.value}",
+    processingDate: ZonedDateTime = epochDateTime,
+    money: Money = money("100", "EUR"),
+    transferDirection: TransferDirection = TransferDirection.OUTGOING,
+    category: TransactionCategory = TransactionCategory.ENTERTAINMENT,
+    status: TransactionStatus = TransactionStatus.COMPLETED,
+    statusCode: TransactionStatusCode = TransactionStatusCode.SUCCESSFUL,
+    attachments: List<String> = emptyList(),
+    cardDescription: String? = null,
+    noteToSelf: String? = null
+) = Transaction(
+    id = id,
+    name = name,
+    processingDate = processingDate,
+    money = money,
+    transferDirection = transferDirection,
+    category = category,
+    status = status,
+    statusCode = statusCode,
+    attachments = attachments,
+    cardDescription = cardDescription,
+    noteToSelf = noteToSelf,
+)
--- domain/src/test/java/com/spiraclesoftware/androidsample/domain/entity/TransactionTest.kt
+++ domain/src/test/java/com/spiraclesoftware/androidsample/domain/entity/TransactionTest.kt
@@ -1,23 +1,16 @@
 package com.spiraclesoftware.androidsample.domain.entity
 
 import com.google.common.truth.Truth.assertThat
-import com.spiraclesoftware.androidsample.domain.epochDateTime
-import com.spiraclesoftware.androidsample.domain.money
+import com.spiraclesoftware.androidsample.domain.transaction
 import org.junit.Test
 
 class TransactionTest {
 
     @Test
     fun `Successful transactions contribute to account balance`() {
-        val transaction = Transaction(
-            TransactionId("1"),
-            "",
-            epochDateTime,
-            money("100", "EUR"),
-            TransferDirection.INCOMING,
-            TransactionCategory.TRANSFERS,
-            TransactionStatus.COMPLETED,
-            TransactionStatusCode.SUCCESSFUL
+        val transaction = transaction(
+            status = TransactionStatus.COMPLETED,
+            statusCode = TransactionStatusCode.SUCCESSFUL
         )
 
         assertThat(transaction.contributesToAccountBalance()).isTrue()
@@ -25,15 +18,9 @@ class TransactionTest {
 
     @Test
     fun `Declined transactions do not contribute to account balance`() {
-        val transaction = Transaction(
-            TransactionId("1"),
-            "",
-            epochDateTime,
-            money("100", "EUR"),
-            TransferDirection.INCOMING,
-            TransactionCategory.TRANSFERS,
-            TransactionStatus.DECLINED,
-            TransactionStatusCode.SPENDING_LIMIT_EXCEEDED
+        val transaction = transaction(
+            status = TransactionStatus.DECLINED,
+            statusCode = TransactionStatusCode.SPENDING_LIMIT_EXCEEDED
         )
 
         assertThat(transaction.contributesToAccountBalance()).isFalse()
