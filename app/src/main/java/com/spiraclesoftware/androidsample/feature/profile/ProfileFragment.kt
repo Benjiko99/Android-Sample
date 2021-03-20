65be7defa164d8b0a4f64ef60fd841c55418e035
Tomáš Beňo <benjiko99@gmail.com>
2021-03-20 15:17:57 +0100

    Setup toolbar

--- app/src/main/java/com/spiraclesoftware/androidsample/feature/profile/ProfileFragment.kt
+++ app/src/main/java/com/spiraclesoftware/androidsample/feature/profile/ProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.spiraclesoftware.androidsample.feature.profile
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import co.zsmb.rainbowcake.koin.getViewModelFromFactory
 import com.spiraclesoftware.androidsample.databinding.ProfileFragmentBinding
 import com.spiraclesoftware.androidsample.framework.StandardFragment
+import com.spiraclesoftware.androidsample.util.DelightUI
 import com.spiraclesoftware.androidsample.feature.profile.ProfileViewState as ViewState
 
 class ProfileFragment :
@@ -16,6 +21,19 @@ class ProfileFragment :
     override fun provideViewModel() = getViewModelFromFactory()
 
     override fun render(viewState: ViewState) {
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() = with(binding) {
+        toolbar.setupWithNavController(findNavController())
+
+        DelightUI.setupToolbarTitleAppearingOnScroll(toolbar, scrollView) {
+            headerView.height
+        }
     }
 
 }
